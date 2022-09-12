3242fbe37a11dc8533d4cd7f3d27c05cfc054e4f
kamalirj <106136963+kamaliraja@users.noreply.github.com>
2022-09-12 13:29:52 +0530

    program using forloops

---
+++ forloops.js
@@ -0,0 +1,17 @@
+ //In this program using for loops to display sum of natural number
+
+let sum = 0;
+const n = 6;
+
+// looping from i = 1 to n
+// in each iteration, i is increased by 1
+
+for (let i = 1; i <= n; i++) {
+    sum += i;                    // sum = sum + i
+}
+
+console.log('Sum of Natural numbers using for loop :', sum);  // prints 21
+
+
+
+
