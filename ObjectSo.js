3275879d5265ac6bac6d9c4a2ed68f92756fd940
kamalirj <106136963+kamaliraja@users.noreply.github.com>
2022-09-16 09:14:37 +0530

    using SO with object and array destructuring

--- ObjectSo.js
+++ ObjectSo.js
@@ -6,8 +6,10 @@ console.log(obj2);
 
 //use the spread operator with Array Destructuring.
 const numbers = [5,10,25,15,"abc",'kamali'];
+
     // Here we assign a,b and c with 5,10 and 25,
     // the rest of the elements will all go to others
+
     const [a,b,c,...others] = numbers;
     console.log(a);
     console.log(b);
