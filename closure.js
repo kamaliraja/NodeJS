83b33b111433a80f00781871b534bbfe39284cd0
kamalirj <106136963+kamaliraja@users.noreply.github.com>
2022-10-18 12:43:30 +0530

    program using closure

--- closure.js
+++ closure.js
@@ -1,6 +1,6 @@
 function fun()  
 {  
-var a = 4; // 'a' is the local variable, created by the fun()  
+var a = 5; // 'a' is the local variable, created by the fun()  
 function innerfun() // the innerfun() is the inner function, or a closure  
 {  
 return a;  
