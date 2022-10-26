aaba7f4f952e59c1756daa884ec27bd165a859a5
kamalirj <106136963+kamaliraja@users.noreply.github.com>
2022-10-26 12:23:09 +0530

    adding elements in array function

---
+++ arrayElements.js
@@ -0,0 +1,16 @@
+ let colors =["blue","green","yellow"];
+let color = "orange";
+const addElement = (...args)=>{
+    let result = [];
+    for (const iterator of args) {
+    
+        result.unshift(iterator);
+    }
+    return result;
+}
+console.log(addElement(...colors,color));  
+
+//using a push method 
+/* let colors =["blue","green","yellow"];
+colors.push("orange");
+console.log(colors);  */
